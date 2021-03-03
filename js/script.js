75db09f101ff5def5f47c7807f6ecae0903ccd6a
Juan Ignacio <>
2021-03-02 21:48:49 -0600

    Agrego entregable 3 del portafolio con el feedback aplicado

--- js/script.js
+++ js/script.js
@@ -7,6 +7,44 @@ function toggleMenu (event) {
   this.classList.toggle('is-active');
   document.querySelector( ".menuppal" ).classList.toggle("is_active");
 }
-
-// event
 menu.addEventListener('click', toggleMenu, false);
+
+
+const formulario = document.querySelector(".form form");
+
+
+const UserUrl = "https://api.github.com/users/JLedezmaC";
+const GithubData = () => {
+  fetch(UserUrl, {
+    method: "GET"
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.status);
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      addData(data)
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
+};
+
+GithubData();
+
+function addData(data){
+  const TotalProyects = document.querySelector(".total-projects a")
+  TotalProyects.innerHTML = `${data.public_repos}`;
+  formulario.addEventListener("submit",()=>{
+    formulario.elements[0].value = "";
+    formulario.elements[1].value = "";
+    formulario.elements[2].value = "";
+    formulario.elements[3].value = "";
+  });
+}
+
+
+
